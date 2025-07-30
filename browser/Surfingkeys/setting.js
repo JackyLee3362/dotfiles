de00976f2a090be8d3378c57cac6635fae96161d
Jackylee <jieli.work@qq.com>
2025-07-30 09:19:50 +0800

    ✨feat(surfingkeys): 新增浏览器 vim 配置

---
+++ browser/Surfingkeys/setting.js
@@ -0,0 +1,26 @@
+const { map, unmap, mapkey } = api;
+// 上个 tab
+// map("H", "E");
+// unmap('E')
+
+// 下个 tab
+// map("L", "R");
+// unmap('R')
+
+// 下半页
+// map("J", "R");
+// unmap('d')
+
+// 上半页
+// map("K", "E");
+// unmap('u')
+
+// OmniSearch
+// map("o", "t");
+// unmap("t");
+
+// 前进 Forward 和返回 Backward
+// map('gl', 'F')
+// map('gh', 'B')
+
+settings.scrollStepSize = 240;
