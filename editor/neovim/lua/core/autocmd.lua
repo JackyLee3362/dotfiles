8478fa2141d5d2810202547af7d2a72f052869b2
lijie206 <lijie206@meituan.com>
2025-09-01 17:02:44 +0800

    🔧conf: 更新 vim 复制高亮

--- editor/neovim/lua/core/autocmd.lua
+++ editor/neovim/lua/core/autocmd.lua
@@ -16,6 +16,15 @@ vim.api.nvim_create_autocmd("FileType", {
     once = true,
 })
 
+-- https://stackoverflow.com/questions/26069278/highlight-copied-area-in-vim
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 100 }
+  end,
+})
 -- 自定义事件都是 VeryLazy 的不同 pattern
 -- vim.api.nvim_create_autocmd("User", {
 --     pattern = "MyVeryLazy",
