287d78e6dc7876bcab6cde77a5d864e26c718b6e
Jackylee <jieli.work@qq.com>
2025-09-07 18:04:57 +0800

    🚧wip: 更新lang rime-custom powershell

--- shell/powershell/profile.ps1
+++ shell/powershell/profile.ps1
@@ -17,8 +17,8 @@ $env:UV_SYSTEM_PYTHON=1
 
 #region conda initialize
 # !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\JACKYLEE\scoop\apps\miniconda3\current\Scripts\conda.exe") {
-    (& "C:\Users\JACKYLEE\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
+# If (Test-Path "C:\Users\JACKYLEE\scoop\apps\miniconda3\current\Scripts\conda.exe") {
+#     (& "C:\Users\JACKYLEE\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+# }
 #endregion
 
