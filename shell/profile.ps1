e7c364d71dbc69c5e23e115a72e9a003916d6c52
Jackylee <jieli.work@qq.com>
2025-09-27 14:31:52 +0800

    ✨feat: 更新 powershell

--- shell/profile.ps1
+++ shell/profile.ps1
@@ -5,7 +5,7 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # 设置别名
 Set-Alias -Name w -Value where.exe
 # Set-Alias -Name open -Value explorer
-Set-Alias -Name init-conda -Value $env:USERPROFILE\Documents\Powershell\conda.ps1
+Set-Alias -Name conda-init -Value $env:USERPROFILE\Documents\Powershell\conda.ps1
 
 
 # 设置 tldr 环境变量
