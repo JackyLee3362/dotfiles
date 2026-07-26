2006fa0bd78561f538935e2cd54e989bd983acff
Jackylee <jieli.work@qq.com>
2026-07-27 01:40:52 +0800

    ✨feat: 更新配置

--- shell/profile.ps1
+++ shell/profile.ps1
@@ -5,8 +5,8 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 # 设置别名
 Set-Alias -Name w -Value where.exe
 # Set-Alias -Name open -Value explorer
-# 删除了 conda
-# Set-Alias -Name conda-init -Value $env:USERPROFILE\Documents\Powershell\conda.ps1
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name vi -Value nvim
 
 # 设置 tldr 环境变量
 $env:TLDR_LANGUAGE="zh"
@@ -14,3 +14,7 @@ $env:TLDR_CACHE_ENABLED=1
 
 # 设置 uv 环境变量
 $env:UV_SYSTEM_PYTHON=1
+
+# linux 定义存储用户特定的配置文件的基准目录
+# 默认值是 $HOME/.config
+# $env:XDG_CONFIG_HOME="$HOME/.config"
